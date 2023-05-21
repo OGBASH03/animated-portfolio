6fa73fbb971402f218ebdb3cdfd7a2c51a9bc25c
OGBASH03 <116046076+OGBASH03@users.noreply.github.com>
2023-05-21 13:35:27 -0400

    Bug-Fix routes

--- sea-react-portfolio/src/components/Sidebar/index.js
+++ sea-react-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import { Link } from 'react-router-dom'
-import LogoS from './../assets/images/logo-s.png'
-import LogoSubtitle from './../assets/images/logo-sub.png'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => (
     <div className='nav-bar'>
